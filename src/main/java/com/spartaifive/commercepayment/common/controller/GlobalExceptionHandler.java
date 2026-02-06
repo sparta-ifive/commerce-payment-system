a9997ded793824633c4c2695e49673bfff0fe843
imprity <imprity041@gmail.com>
2026-02-06 23:53:04 +0900

    feat: 전역 exception을 핸들링 할 수있는 controller를 추가.
    일단은 이렇게 나둔뒤 나중에 상세 에러를 추가 하면 될 거 같습니다.

---
+++ src/main/java/com/spartaifive/commercepayment/common/controller/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.spartaifive.commercepayment.common.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
