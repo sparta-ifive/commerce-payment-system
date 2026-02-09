acb51ddf8aa4d0202e84a2d4bf96f7ac4ce72845
imprity <imprity041@gmail.com>
2026-02-09 11:45:01 +0900

    comment: 상품 조회에 대한 주석 수정
    상품은 로그인 한 사람만 조회를 할 수 있는 방향으로 결정 되었으므로 상품
    주석을 공개로 해야하는지에 대한 논의 주석은 삭제했습니다.

--- src/main/java/com/spartaifive/commercepayment/domain/product/controller/ProductController.java
+++ src/main/java/com/spartaifive/commercepayment/domain/product/controller/ProductController.java
@@ -21,23 +21,6 @@ import lombok.RequiredArgsConstructor;
 public class ProductController {
     private final ProductService productService;
 
-    // TODO: 저희 설계에 따르면 상품 조회 API는 누구나 조회 할 수 있는 것이 맞습니다.
-    //
-    // 하지만 현재 SecurityConfig를 보면 /api/public 아래에 있는 api들만
-    // 공개적을 들어가도 록 설정 되어 있습니다.
-    //
-    // 그렇다면 저희에게 옵션은 두가지 입니다.
-    //
-    // 1. 공개 api를 public 아래로 둔다
-    //  그렇게 된다면 /api/products/{productId} 는
-    //  /api/public/products/{productId}가 되겠죠.
-    //
-    // 2. api 주소는 그대로 두고 SecurityConfig를 수정한다.
-    //
-    // 이에 대해서 저혼자 결정 할 수 있는 문제가 아니므로 일단은 
-    // login을 해야 상품 조회가 가능합니다.
-    // 이에 대해서는 추가로 팀원들과의 논의가 필요 할 듯 합니다.
-
     @GetMapping("/api/products/{productId}")
     public ResponseEntity<DataResponse<GetProductResponse>> getProduct(
             @PathVariable Long productId
